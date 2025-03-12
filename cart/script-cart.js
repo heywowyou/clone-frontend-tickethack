fef083e199f949cb9c655907bff7b9021535f12c
Warren <Warren9555@gmail.com>
2025-03-12 10:43:22 +0100

    gg

--- cart/script-cart.js
+++ cart/script-cart.js
@@ -30,6 +30,7 @@ async function getTrips() {
     console.log("User data:", user);
 
     cart = user || []; // Stocke le panier
+    console.log("nv tableau; ", cart);
     displayCart(); // Met à jour l'affichage
     return cart;
   } catch (error) {
